2df68f3259b6d58b9bfed81f75df13cd018e8044
Ollie Preiss <76745659+OlliePreiss@users.noreply.github.com>
2024-10-01 13:43:42 +0100

    basic express file created

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res. send("Hello, world!"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`My first Express app listneing on ${PORT}!`));
